45d7fe054c277b2307bf280a30f55b1c939a27ef
Soufianelotfi-lab <lotfisoufiane2002@gmail.com>
2025-11-26 20:12:12 +0100

    Add .gitignore

--- CMakeVS2022.bat
+++ CMakeVS2022.bat
@@ -1,5 +1,5 @@
 @echo off
-
+mkdir bin
 cd bin
 cmake ..\ -G "Visual Studio 17 2022" 
 pause
--- src/yuv2RGB.c
+++ src/yuv2RGB.c
@@ -9,6 +9,7 @@
 */
 
 #include "yuv2RGB.h"
+#include <omp.h>
 
 #define YUV2RGB_COEF_R  1.13983
 #define YUV2RGB_COEF_G1 0.39465
@@ -19,7 +20,9 @@
 
 void yuv2rgb(int width, int height, unsigned char *y, unsigned char *u, unsigned char *v, unsigned char *rgb){
     int i,j;
+#pragma omp parallel for num_threads(8) private (j, i)
     for(i=0; i< height; i++){
+
         for(j=0; j < width; j++){
 			int idx = i*width + j;
 			int idxUV = i/2*width/2 + j/2;
