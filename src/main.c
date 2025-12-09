c9b7c33be4f55603f0d9d6b46a1d07833e2ec569
Lotfi Soufiane <soufiane.lotfi@insa-rennes.fr>
2025-12-09 14:02:46 +0100

    Amelioration

--- src/main.c
+++ src/main.c
@@ -22,7 +22,7 @@ int stopThreads = 0;
 
 int main(void) {
 	printf("Stereo Matching App\n");
-	omp_set_num_threads(14);
+	omp_set_num_threads(16);
 	// Open YUV Files (left & right)
 	initReadYUV(0, WIDTH, HEIGHT);
 	initReadYUV(1, WIDTH, HEIGHT);
